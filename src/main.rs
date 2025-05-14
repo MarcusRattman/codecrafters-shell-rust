b497a52c985d26224477600e0ce1faffd40d5079
marcusrattman <zedrattman@gmail.com>
2025-05-15 04:07:43 +0700

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use std::io::{self, Write};
 
 fn main() {
-    let mut input = String::new();
     loop {
+        let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
