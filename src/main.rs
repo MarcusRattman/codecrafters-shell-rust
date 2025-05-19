5d30155c09624fd9351aa33a78368e333f90037f
marcusrattman <zedrattman@gmail.com>
2025-05-20 00:33:02 +0700

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -58,12 +58,7 @@ fn parse_command(input: &str) -> Result<String, CommandParseError> {
     });
 
     if args_vec.is_empty() {
-        args_vec.push(
-            args.split(" ")
-                .map(|arg| arg.trim())
-                .collect::<Vec<&str>>()
-                .join(" "),
-        );
+        args_vec.push(args.split_whitespace().collect());
     }
 
     match command {
