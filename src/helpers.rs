0c52edea08e4bdb0f21a739033656b7efe608273
marcusrattman <zedrattman@gmail.com>
2025-05-20 23:16:37 +0700

    cock

--- src/helpers.rs
+++ src/helpers.rs
@@ -36,8 +36,8 @@ fn parse_args(args: &str) -> Vec<String> {
         match c {
             '\\' => {
                 if let Some(&next_char) = chars.peek() {
-                    if (SPECIAL_CHARACTERS.contains(&next_char) || !in_double_quotes)
-                        && !in_single_quotes
+                    if !in_single_quotes
+                        && (SPECIAL_CHARACTERS.contains(&next_char) || !in_double_quotes)
                     {
                         chars.next();
                         current_arg.push(next_char);
