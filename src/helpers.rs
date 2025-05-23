de0d6e26c4d276c4cd17524f4fbe09938389c0d6
marcusrattman <zedrattman@gmail.com>
2025-05-24 00:12:39 +0700

    test

--- src/helpers.rs
+++ src/helpers.rs
@@ -81,7 +81,7 @@ fn exec_command(mut to_match: Vec<String>) -> Result<String, CommandParseError> 
         _ => {
             let exec = run_binary(command, args);
             match exec {
-                Ok(s) => Ok(s.stdout),
+                Ok(s) => Ok(s.stderr),
                 Err(e) => Err(CommandParseError::ComposableError(e)),
             }
         }
